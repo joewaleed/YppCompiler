f4b91f1518e724a4e790780038d595e2c30bcd57
joewaleed <yousef.walid1105@gmail.com>
2026-03-15 00:27:47 +0200

    Add custom exit button and improve menu layout.

--- Ypp Compiler/MainWindow.xaml.cs
+++ Ypp Compiler/MainWindow.xaml.cs
@@ -21,6 +21,25 @@ namespace Ypp_Compiler {
             this.MaxWidth = SystemParameters.WorkArea.Width;
             this.MaxHeight = SystemParameters.WorkArea.Height;
             MessageBox.Show("Thank you for trying ypp compiler early access.\nPlease note that this is v0.1 and some functionalities might not work.\nEnjoy!", "Welcome",MessageBoxButton.OK,MessageBoxImage.Information);
+            TextEditor.Text = """
+                int x = 10;
+                ?(x>y){
+                     text message = "x is greater than y";
+                } else {
+                    text message = "x is not greater than y";
+                }
+                repeat 5 {
+                    x += 1;
+                }
+
+                repeat {
+                    x += 1;
+                } until x == 20;
+
+                repeat x > 30 {
+                    x += 1;
+                }
+                """;
         }
 
         public class TokenRow {
@@ -40,6 +59,22 @@ namespace Ypp_Compiler {
             
 
             Scanner_Output.ItemsSource = tokenRowsList;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void Documentation_Click(object sender, RoutedEventArgs e) {
+            string url = "https://github.com/joewaleed/YppCompiler";
+            try {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            } catch (Exception ex) {
+                MessageBox.Show($"Unable to open documentation: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
