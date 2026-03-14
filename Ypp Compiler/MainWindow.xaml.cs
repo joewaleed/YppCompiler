a5b1d46da4dbf73f37c4f59cda2c671f0219249a
joewaleed <yousef.walid1105@gmail.com>
2026-03-14 21:36:42 +0200

    Add lexical scanner UI with regex token classification.

--- Ypp Compiler/MainWindow.xaml.cs
+++ Ypp Compiler/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Data;
 
 namespace Ypp_Compiler {
     /// <summary>
@@ -18,6 +20,26 @@ namespace Ypp_Compiler {
             InitializeComponent();
             this.MaxWidth = SystemParameters.WorkArea.Width;
             this.MaxHeight = SystemParameters.WorkArea.Height;
+            MessageBox.Show("Thank you for trying ypp compiler early access.\nPlease note that this is v0.1 and some functionalities might not work.\nEnjoy!", "Welcome",MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+
+        public class TokenRow {
+            public string Token { get; set; }
+            public string Lexeme { get; set; }
+        }
+
+        private void Run_Click(object sender, RoutedEventArgs e) {
+            MatchCollection matches = TextEditor.Text.GetMatch(); //Note : this is an extension method defined in RegexValues.cs
+            List<TokenRow> tokenRowsList = new List<TokenRow>();
+
+            foreach (Match match in matches)
+                        tokenRowsList.Add(new TokenRow { 
+                            Token = match.Value,
+                            Lexeme = match.Value.IsMatch() //This is another extension method defined in RegexValues.cs
+                        });
+            
+
+            Scanner_Output.ItemsSource = tokenRowsList;
         }
     }
 }
---
+++ Ypp Compiler/RegexValues.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Ypp_Compiler
+{
+    public static class RegexValues
+    {
+        public static string keywords = @"\b(int|text|\?|else|repeat|until)\b";
+        public static string identifiers = @"\b[a-zA-Z_][a-zA-Z0-9]*\b";
+        public static string numbers = @"\b[0-9]+\b";
+        public static string operators = @"==|<>|\+|\-|\*|/";
+        public static string Assignment = @"\+=|-=|\*=|=";
+        public static string symbols = @"[{}()]";
+        public static string semicolumn = ";";
+
+        public static string MasterPattern = $"{keywords}|{identifiers}|{numbers}|{operators}|{Assignment}|{symbols}|{semicolumn}";
+
+        public static MatchCollection GetMatch(this string text) {
+            return Regex.Matches(text, MasterPattern);
+        }
+
+        public static string IsMatch(this string text) {
+            return Regex.IsMatch(text, keywords) ? "Keyword" : 
+                   Regex.IsMatch(text, identifiers) ? "Identifier" :
+                   Regex.IsMatch(text, numbers) ? "Number" :
+                   Regex.IsMatch(text, operators) ? "Operator" :
+                   Regex.IsMatch(text, Assignment) ? "Assignment" :
+                   Regex.IsMatch(text, symbols) ? "Symbol" :
+                   Regex.IsMatch(text, semicolumn) ? "Semicolon" : 
+                   "Unknown";
+        }
+    }
+}
